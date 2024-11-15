6a63974c512a86c8c6ada10ff85751d9301589d0
Adrien BRUNETON <adrien.bruneton@cea.fr>
2024-11-15 11:05:52 +0100

    Reactivating doxygen

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -89,7 +89,7 @@ html_favicon = 'favicon.ico'
 # Generating doxygen from here - this will run first, before RST processing from sphinx:
 import subprocess as sp
 
-if 0:
+if 1:
   print("@@@ About to generate doxygen!!")
   sp.call("cd srcs/doxy; doxygen", shell=True)
   rtd = os.environ.get("READTHEDOCS_OUTPUT", "build")
